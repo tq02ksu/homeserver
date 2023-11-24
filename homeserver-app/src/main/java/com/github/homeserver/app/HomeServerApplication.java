c325b5103ff38a15ee81504f226d9b5ba6bcf8f6
tq02ksu <tq02ksu@gmail.com>
2023-11-24 23:31:54 +0800

    init commit

---
+++ homeserver-app/src/main/java/com/github/homeserver/app/HomeServerApplication.java
@@ -0,0 +1,12 @@
+package com.github.homeserver.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HomeServerApplication.class, args);
+  }
+}
